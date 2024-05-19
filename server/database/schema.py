c7b7365e34cb37e947ff4046119306dae7ee93c2
Steven Nguyen <snguyen8@uci.edu>
2024-05-18 19:45:04 -0700

    removed account_type from schema and from db_controller

--- server/database/schema.py
+++ server/database/schema.py
@@ -16,32 +16,14 @@ def create_db():
     ''')
 
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS AccountType (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL UNIQUE
-        );
-    ''')
-
-    cursor.execute('''
         CREATE TABLE IF NOT EXISTS Account (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id INTEGER NOT NULL,
-            account_type_id INTEGER NOT NULL,
             balance INTEGER NOT NULL,
-            FOREIGN KEY (account_type_id) REFERENCES AccountType(id),
             FOREIGN KEY (user_id) REFERENCES User(id)
         );
     ''')
 
-    # insert initial records into reference tables
-    # have to end with a comma to ensure it is a tuple, not a string
-    account_types_records = [
-        ('Checking',), 
-        ('Savings',)
-    ]
-
-    cursor.executemany('INSERT INTO AccountType (name) VALUES (?) ON CONFLICT DO NOTHING;', account_types_records)
-    
     db_connection.commit()
     db_connection.close()
 
