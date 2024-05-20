5efcc55da80c800a61bcec4b81b7495bf86d2960
Steven Nguyen <snguyen8@uci.edu>
2024-05-19 17:36:02 -0700

    installed flask-login dependency

--- server/main.py
+++ server/main.py
@@ -1,8 +1,8 @@
 from flask import Flask
+from flask_login import LoginManager
 from database.schema import create_db
 from api.user_routes import user
 from api.account_routes import account
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 flask = Flask(__name__)
 login_manager = LoginManager(flask)
